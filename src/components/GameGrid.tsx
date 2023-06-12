e6f6233dd8f1417555ab949a823b5d9bb539f68b
Faisal <md.faisal1899@yahoo.com>
2023-06-12 12:32:58 +0600

    display platform icons

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,4 +1,6 @@
+import { SimpleGrid } from '@chakra-ui/react';
 import useGames from '../hooks/useGames';
+import GameCard from './GameCard';
 
 function GameGrid() {
     const { games, error } = useGames();
@@ -6,11 +8,15 @@ function GameGrid() {
     return (
         <>
             {error && <p>Couldn't fetch game data.</p>}
-            <ul>
+            <SimpleGrid
+                columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
+                padding={10}
+                spacing={10}
+            >
                 {games.map((game) => (
-                    <li key={game.id}>{game.name}</li>
+                    <GameCard key={game.id} game={game} />
                 ))}
-            </ul>
+            </SimpleGrid>
         </>
     );
 }
